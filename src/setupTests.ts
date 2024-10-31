2df0de2853bb1b4399010a8b2dc510b70b304e1d
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-10-31 19:30:58 +1300

    chore: cleanup

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,6 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like: `expect(element).toHaveTextContent(/react/i)`
 
+// @ts-ignore
 import * as React from "react";
 import "@testing-library/jest-dom/vitest";
 import "@testing-library/jest-dom/matchers";
